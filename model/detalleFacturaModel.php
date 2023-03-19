fc78e70d18fe3ef55ce957b10e3cc96f76b67369
lary12-code <105029668+lary12-code@users.noreply.github.com>
2023-03-19 12:35:40 -0600

    reporte nuevo  ventas 2

--- model/detalleFacturaModel.php
+++ model/detalleFacturaModel.php
@@ -47,8 +47,9 @@ static public function mdlMostrarDetalleporIdFactura($tabla, $item, $valor){
 		
 		if($item != null){
 
-			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = $item");
+			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item =:$item");
 
+            $stmt -> bindParam(":".$item, $valor, PDO::PARAM_STR); 
 			$stmt -> execute();
 
 			return $stmt -> fetchAll();

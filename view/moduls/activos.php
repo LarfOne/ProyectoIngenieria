5e785d47641c27f8b30424b2658b246265cdf0ed
Ruamer06 <estramaste72@gmail.com>
2023-05-01 15:05:52 -0600

    Tabulaciones

--- view/moduls/activos.php
+++ view/moduls/activos.php
@@ -9,46 +9,44 @@
 <div id= "container pt-4" style="margin-top: 100px;">
 
   <div class="container mt-3">
-      <h2 style="text-align:left; font-family: 'Roboto Condensed', sans-serif !important;">Control de Activos</h2>
+    <h2 style="text-align:left; font-family: 'Roboto Condensed', sans-serif !important;">Control de Activos</h2>
       
       <button class="btn btn-primary btnAgregarAct" data-bs-toggle="modal" data-bs-target="#modalAddActivo" style="text-align:center; font-family: 'Roboto Condensed', sans-serif !important;">
-            Agregar Activos
-        </button>
+        Agregar Activos
+      </button>
 
         <div class="table-responsive roboto">
           <table class="table" id="tabla" data-sort="table">
             <thead>
-                  <tr>
-                        <th>Codigo</th>
-                        <th>Sucursal</th>
-                        <th>Descripcion</th>
-                        <th>Estado</th>
-                        <th>Empleado</th>
-                        <th>Acciones</th>
-                      
-                  </tr>
+              <tr>
+                <th>Codigo</th>
+                <th>Sucursal</th>
+                <th>Descripcion</th>
+                <th>Estado</th>
+                <th>Empleado</th>
+                <th>Acciones</th>       
+              </tr>
             </thead>
 
             <tbody>
 
-                  <?php
-                  $item = null;
-                  $valor = null;
+              <?php
+              $item = null;
+              $valor = null;
                   
-                  $activos = ControllerActivos::ctrShowActivo($item, $valor);
+              $activos = ControllerActivos::ctrShowActivo($item, $valor);
             
-
-            foreach($activos as $key => $activo) {
-              $sucursal = ControllerSucursal::ctrNameSucursal($activo['idSucursal']);
-              $user = ControllerUser::ctrNameUser($activo['empleado_id'])?>
-            <tr>
+              foreach($activos as $key => $activo) {
+                $sucursal = ControllerSucursal::ctrNameSucursal($activo['idSucursal']);
+                $user = ControllerUser::ctrNameUser($activo['empleado_id'])?>
+              
+              <tr>
 
                 <td><?php echo $activo['codigo']; ?></td>
                 <td><?php echo $sucursal['nombre']; ?></td>
                 <td><?php echo $activo['descripcion']; ?></td>
                 <td><?php echo $activo['estado']; ?></td>
                 
-
                 <?php if ($activo['empleado_id']!=NULL) { ?>
                   <td><?php echo $user['nombre']." ".$user['apellidos']; ?></td>
                 <?php } ?>
@@ -57,135 +55,101 @@
                 <td>Libre</td>
                 <?php } ?>
 
-
-
-
-  
-
-
                 <td>
-
                   <div class="btn-group">
-                      <button style="margin: 5px" class="btn btn-warning btnUpdate btnUpdateActivo" codigo=<?php echo $activo['codigo']; ?>
-                      data-bs-toggle="modal" data-bs-target="#modalUpdateActivo"><i class="fa fa-pencil"></i></button>
+                    <button style="margin: 5px" class="btn btn-warning btnUpdate btnUpdateActivo" codigo=<?php echo $activo['codigo']; ?>
+                      data-bs-toggle="modal" data-bs-target="#modalUpdateActivo"><i class="fa fa-pencil"></i>
+                    </button>
                       
-                      <button style="margin: 5px" class="btn btn-danger btnDelete btnDeleteActivo" codigo=<?php echo $activo['codigo']; ?>
-                      ><i class="fa fa-times"></i></button>
+                    <button style="margin: 5px" class="btn btn-danger btnDelete btnDeleteActivo" codigo=<?php echo $activo['codigo']; ?>
+                      ><i class="fa fa-times"></i>
+                    </button>
                   </div>
-
                 </td>
 
+              </tr>
 
-            </tr>
+              <?php } ?>
 
-            <?php } ?>
-
-          </tbody>
+            </tbody>
 
           </table>
-            </div>
-    </div>
+        </div>
   </div>
-
 </div>
 
-
 <!--MODAL PARA AGREGAR ACTIVOS-->
-
 
 <div class="modal fade" id="modalAddActivo" role="dialog">
   <div class="modal-dialog">
     <div class="modal-content">
 
-
       <form role="form" method="POST" enctype="multipart/form-data">
-
         <div class="modal-header modalHeaderColor">
           <h4 class="modal-title">Agregar Activos</h4>
         </div>
 
-    </br>
         <div class="modal-body">
-
           <div class="box-body modalAct">
 
-            <!--AGREGAR id de sucursal-->
-
+            <!--AGREGAR ID DE SUCURSAL EN LA QUE ESTARÁ EL ACTIVO-->
             <?php
-                  $item = null;
-                  $valor = null;
-                  $sucursal = ControllerSucursal::ctrShowSucursal($item, $valor);
+              $item = null;
+              $valor = null;
+              $sucursal = ControllerSucursal::ctrShowSucursal($item, $valor);
             ?>
 
             <div class="form-group">
-
               <div class="input-group">
-
-                  <span class="input-group-addon"><i class="fa fa-building"></i></span>
-                  <!--<input type="text" class="form-control input-lg" name="idSucursal" placeholder="Ingresar id de sucursal" required>-->
+                <span class="input-group-addon"><i class="fa fa-building"></i></span>
+                <!--<input type="text" class="form-control input-lg" name="idSucursal" placeholder="Ingresar id de sucursal" required>-->
                   <select class="form-control input-lg" id="idSucursal" style="border-radius: 5px;" name="idSucursal">
-                              <?php foreach ($sucursal as $sucursal1) { ?>
-                                    <option value=<?php echo $sucursal1['codigo'] ?>><?php echo $sucursal1['nombre'] ?></option>
-                              <?php } ?>
+                  <option value="" disabled selected>Selecciona una sucursal</option>
+                  <?php foreach ($sucursal as $sucursal1) { ?>
+                      <option value=<?php echo $sucursal1['codigo'] ?>><?php echo $sucursal1['nombre'] ?></option>
+                    <?php } ?>
                   </select>
               </div>
-
             </div>
 
-            <!--AGREGAR DE Descripcion-->
+            <!--AGREGAR DESCRIPCION DEL ACTIVO-->
             <div class="form-group">
-
               <div class="input-group">
-                  <span class="input-group-addon"><i class="fa fa-map"></i></span>
-                  <input type="text" style="border-radius: 5px;" class="form-control input-lg" name="descripcion" placeholder="Ingresar descripción" required>
-
+                <span class="input-group-addon"><i class="fa fa-map"></i></span>
+                <input type="text" style="border-radius: 5px;" class="form-control input-lg" name="descripcion" placeholder="Ingresar descripción" required>
               </div>
-
             </div>
 
-            <!--AGREGAR DE estado-->
+            <!--AGREGAR ESTADO DEL ACTIVO-->
             <div class="form-group">
-
               <div class="input-group">
-
-                  <span class="input-group-addon"><i class="fa fa-phone"></i></span>
-                  <input type="text" style="border-radius: 5px;" class="form-control input-lg" name="estado" placeholder="Ingresar estado" required>
-
+                <span class="input-group-addon"><i class="fa fa-phone"></i></span>
+                <input type="text" style="border-radius: 5px;" class="form-control input-lg" name="estado" placeholder="Ingresar estado" required>
               </div>
-
             </div>
 
+            <!--AGREGAR EMPLEADO ENCARGADO DEL ACTIVO-->
             <?php
-                  $item = null;
-                  $valor = null;
-                  $user = ControllerUser::ctrShowUser($item, $valor);
+              $item = null;
+              $valor = null;
+              $user = ControllerUser::ctrShowUser($item, $valor);
             ?>
 
-            <!--AGREGAR DE empleadoid-->
             <div class="form-group">
-
               <div class="input-group">
-
-                  <span style="border-radius: 5px;" class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                  <!--<input type="text" class="form-control input-lg" name="empleado_id" placeholder="Ingresar cédula del empleado">-->
-                  <select class="form-control input-lg" id="empleado_id" name="empleado_id" style="border-radius: 5px;">
-                            <?php foreach ($user as $user1) { ?>
-
-                              <?php if ($user1['estado']!='Inactivo') { ?>
-
-                                <option value=<?php echo $user1['cedula'] ?>><?php echo $user1['nombre']." ".$user1['apellidos'] ?></option>
-
-                                <?php } ?>                                
-                            <?php } ?>
-                  </select>
-
+                <span style="border-radius: 5px;" class="input-group-addon"><i class="fa fa-envelope"></i></span>           
+                <select class="form-control input-lg" id="empleado_id" name="empleado_id" style="border-radius: 5px;">
+                  <option value="" disabled selected>Selecciona un empleado</option>
+                  <?php foreach ($user as $user1) { ?>
+                      <?php if ($user1['estado']!='Inactivo') { ?>
+                        <option value=<?php echo $user1['cedula'] ?>><?php echo $user1['nombre']." ".$user1['apellidos'] ?></option>
+                      <?php } ?>                                
+                    <?php } ?>
+                </select>
               </div>
-
             </div>
-
 
           </div>
-
         </div>
 
         <div class="modal-footer">
@@ -194,14 +158,12 @@
         </div>
 
         <?php
-
           $addActivo = new ControllerActivos;
           $addActivo->ctrCreateActivo();
-
         ?>
 
       </form>
-      
+
     </div>
   </div>
 </div>
@@ -212,91 +174,68 @@
   <div class="modal-dialog">
     <div class="modal-content">
 
-
       <form role="form" method="POST" enctype="multipart/form-data">
 
         <div class="modal-header modalHeaderColor" >
           <h4 class="modal-title">Editar Activos</h4>
         </div>
 
-
         <div class="modal-body">
-
           <div class="box-body modalAct">
 
-                        <!--MODIFICAR DE Codigo-->
-                        <div class="form-group">
+            <!--MODIFICAR DE CODIGO QUE ES SOLO VISIBLE-->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                <input type="text" style="border-radius: 5px;" class="form-control input-lg" id="codigom" name="codigom" value="Ingresar el codigo" readonly>
+              </div>
+            </div>
 
-                            <div class="input-group">
+            <!--MODIFCAR ID DE SUCURSAL-->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-building"></i></span>
+                  <select class="form-control input-lg" id="idSucursalm" name="idSucursalm" style="border-radius: 5px;">
+                    <option value="" disabled selected>Selecciona la nueva sucursal</option>
+                    <?php foreach ($sucursal as $sucursal1) { ?>
+                      <option value=<?php echo $sucursal1['codigo'] ?>><?php echo $sucursal1['nombre'] ?></option>
+                    <?php } ?>
+                  </select>
+              </div>
+            </div>
 
-                                <span class="input-group-addon"><i class="fa fa-key"></i></span>
-                                <input type="text" style="border-radius: 5px;" class="form-control input-lg" id="codigom" name="codigom" value="Ingresar el codigo" readonly>
-                            </div>
+            <!--MODIFICAR DE DESCRIPCION DEL ACTIVO-->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-map"></i></span>
+                <input type="text" class="form-control input-lg" id="descripcionm" name="descripcionm" style="border-radius: 5px;" value="Ingresar descripción" required>
+              </div>
+            </div>
 
-                        </div>
-
-                        <!--MODIFCAR DE id sucursal-->
-                        <div class="form-group">
-
-                            <div class="input-group">
-
-                                <span class="input-group-addon"><i class="fa fa-building"></i></span>
-                                <!--<input type="text" class="form-control input-lg" id="idSucursalm" name="idSucursalm" value="Ingresar id de sucursal" required>-->
-                                <select class="form-control input-lg" id="idSucursalm" name="idSucursalm" style="border-radius: 5px;">
-                                  <?php foreach ($sucursal as $sucursal1) { ?>
-                                        <option value=<?php echo $sucursal1['codigo'] ?>><?php echo $sucursal1['nombre'] ?></option>
-                                  <?php } ?>
-                                </select>
-
-                            </div>
-
-                        </div>
-
-                        <!--MODIFICAR DE Descripcion-->
-                        <div class="form-group">
-
-                            <div class="input-group">
-
-                                <span class="input-group-addon"><i class="fa fa-map"></i></span>
-                                <input type="text" class="form-control input-lg" id="descripcionm" name="descripcionm" style="border-radius: 5px;" value="Ingresar descripción" required>
-
-                            </div>
-
-                        </div>
-
-
-                        <!--MODIFICAR DE estado-->
-                        <div class="form-group">
-
-                            <div class="input-group">
-
-                                <span class="input-group-addon"><i class="fa fa-phone"></i></span>
-                                <input type="text" class="form-control input-lg" id="estadom" name="estadom" style="border-radius: 5px;" value="Ingresar estado" required>
-
-                            </div>
-
-                        </div>
-                        
-                        <!--MODIFICAR DE empleado_id-->
-                        <div class="form-group">
-
-                            <div class="input-group">
-
-                                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                                <!--<input type="text" class="form-control input-lg" id="empleado_idm" name="empleado_idm" value="Ingresar cédula del empleado" required>-->
-                                <select class="form-control input-lg" id="empleado_idm" name="empleado_idm" style="border-radius: 5px;">
-                                          <?php foreach ($user as $user1) { ?>
-                                              <?php if ($user1['estado']!='Inactivo') { ?>
-                                                <option value=<?php echo $user1['cedula'] ?>><?php echo $user1['nombre']." ".$user1['apellidos'] ?></option>
-                                              <?php } ?>
-                                          <?php } ?>
-                                </select>
-                            </div>
-
-                        </div>
+            <!--MODIFICAR DE ESTADO DEL ACTIVO-->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-phone"></i></span>
+                <input type="text" class="form-control input-lg" id="estadom" name="estadom" style="border-radius: 5px;" value="Ingresar estado" required>
+              </div>
+            </div>
+            
+            <!--MODIFICAR DE EMPLEADO ENCARGADO-->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                  <select class="form-control input-lg" id="empleado_idm" name="empleado_idm" style="border-radius: 5px;">
+                    <option value="" disabled selected>Selecciona el nuevo empleado a cargo</option>
+                    <?php foreach ($user as $user1) { ?>
+                      <?php if ($user1['estado']!='Inactivo') { ?>
+                        <option value=<?php echo $user1['cedula'] ?>><?php echo $user1['nombre']." ".$user1['apellidos'] ?></option>
+                      <?php } ?>
+                    <?php } ?>
+                  </select>
+              </div>
+            </div>
 
           </div>
-
         </div>
 
             <div class="modal-footer">
@@ -316,9 +255,6 @@
 </div>
 
 <?php
-  
   $deleteActivo = new ControllerActivos;
-
   $deleteActivo -> ctrDeleteActivo() ;
-
 ?>
--- view/moduls/categories.php
+++ view/moduls/categories.php
@@ -7,71 +7,63 @@
 <script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap4.min.js"></script>
 
 <div id= "container pt-4" style="margin-top: 100px;">
+  <div class="container mt-3">
 
-<div class="container mt-3">
-  <h2 style="text-align:left; font-family: 'Roboto Condensed', sans-serif !important;">Control de Categorías</h2>
-
+    <h2 style="text-align:left; font-family: 'Roboto Condensed', sans-serif !important;">Control de Categorías</h2>
     <button class="btn btn-primary btnAgregarCat" data-bs-toggle="modal" data-bs-target="#modalAddCategories">
-        Agregar Categoría
+      Agregar Categoría
     </button>
-  <div class="box-body">
-  <div class="table-responsive roboto">
-  <table class="table" id="tabla" data-sort="table">
-    <thead>
-                <tr>
-                    <th>Código</th>
-                    <th>Nombre</th>
-                    <th>Acciones</th>
-                    
-                </tr>
-                </thead>
 
-                <tbody>
+    <div class="box-body">
+      <div class="table-responsive roboto">
+        <table class="table" id="tabla" data-sort="table">
+          <thead>
+            <tr>
+              <th>Código</th>
+              <th>Nombre</th>
+              <th>Acciones</th> 
+            </tr>
+          </thead>
 
-    <?php
-    $item = null;
-    $valor = null;
-    
-    $categories = ControllerCategories::ctrShowCategories($item, $valor);
-    
+          <tbody>
 
-    foreach($categories as $key => $categories1) { ?>
-    <tr>
+            <?php
+            $item = null;
+            $valor = null;
+            
+            $categories = ControllerCategories::ctrShowCategories($item, $valor);
+            
+            foreach($categories as $key => $categories1) { ?>
 
-        <td><?php echo $categories1['codigo']; ?></td>
-        <td><?php echo $categories1['nombre']; ?></td>
-  
+              <tr>
+                <td><?php echo $categories1['codigo']; ?></td>
+                <td><?php echo $categories1['nombre']; ?></td>
 
-        <td>
-
-          <div class="btn-group">
-              <button style="margin: 5px" class="btn btn-warning btnUpdate btnUpdateCategories" idCategories = <?php echo $categories1['codigo']; ?>
-              data-bs-toggle="modal" data-bs-target="#modalUpdateCategories"><i class="fa fa-pencil"></i></button>
+                <td>
+                  <div class="btn-group">
+                    <button style="margin: 5px" class="btn btn-warning btnUpdate btnUpdateCategories" idCategories = <?php echo $categories1['codigo']; ?>
+                      data-bs-toggle="modal" data-bs-target="#modalUpdateCategories"><i class="fa fa-pencil"></i>
+                    </button>
+                      
+                    <button style="margin: 5px" class="btn btn-danger btnDelete btnDeleteCategories" codigoM = <?php echo $categories1['codigo']; ?>
+                      ><i class="fa fa-times"></i>
+                    </button>
+                  </div>
+                </td>
+              </tr>
               
-              <button style="margin: 5px" class="btn btn-danger btnDelete btnDeleteCategories" codigoM = <?php echo $categories1['codigo']; ?>
-              ><i class="fa fa-times"></i></button>
-          </div>
+            <?php } ?>
 
-        </td>
+          </tbody>
 
-
-    </tr>
-
-    <?php } ?>
-
-
-    </tbody>
-
-    </table>
+        </table>
+      </div>
     </div>
   </div>
 </div>
 
-</div>
-
 
 <!--MODAL PARA AGREGAR CATEGORIAS-->
-
 
 <div class="modal fade" id="modalAddCategories" role="dialog">
   <div class="modal-dialog">
@@ -80,17 +72,8 @@
 
       <form role="form" method="POST" enctype="multipart/form-data">
 
-<<<<<<< HEAD
-        <div class="modal-header" style="background:#3c8dbc; color:white">
-<<<<<<< HEAD
-          <h4 class="modal-title">Agregar Categorías</h4>
-=======
-          <h4 class="modal-title" style="text-align:left; font-family: 'Roboto Condensed', sans-serif !important;">Agregar Categories</h4>
->>>>>>> origin/laryBranch
-=======
         <div class="modal-header modalHeaderColor">
           <h4 class="modal-title">Agregar Categorías</h4>
->>>>>>> origin/laryBranch
         </div>
 
     </br>
@@ -153,17 +136,9 @@
 
       <form role="form" method="POST" enctype="multipart/form-data">
 
-<<<<<<< HEAD
-        <div class="modal-header" style="background:#3c8dbc; color:white">
-<<<<<<< HEAD
-          <h4 class="modal-title">Editar Categorías</h4>
-=======
-          <h4 class="modal-title" style="text-align:left; font-family: 'Roboto Condensed', sans-serif !important;">Editar Categories</h4>
->>>>>>> origin/laryBranch
-=======
+
         <div class="modal-header modalHeaderColor" >
           <h4 class="modal-title">Editar Categorías</h4>
->>>>>>> origin/laryBranch
         </div>
 
 

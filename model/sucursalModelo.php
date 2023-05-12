33cec1dd9394dbfecda5ea28d642c09453092618
lary12-code <105029668+lary12-code@users.noreply.github.com>
2023-05-12 11:56:04 -0600

    documentacion de codigo

--- model/sucursalModelo.php
+++ model/sucursalModelo.php
@@ -4,10 +4,11 @@ require_once "conexion.php";
 
 class Sucursal{             
 
-
+    //mdlNameSucursal que acepta un parámetro $valor
     static public function mdlNameSucursal($valor){
         //SELECT nombre FROM `sucursal` WHERE codigo = 1
-
+        //Realiza una consulta a la tabla de base de datos llamada sucursal y recupera todas las columnas donde la columna codigo
+        // es igual al valor del parámetro $valor.
         $sentenciaSQL = Conexion::conectar()->prepare("SELECT * FROM sucursal WHERE codigo = :codigo");
 
         $sentenciaSQL -> bindParam(":codigo", $valor, PDO::PARAM_STR);
@@ -16,9 +17,10 @@ class Sucursal{
 
         return $sentenciaSQL -> fetch();
     }
-
+    //mdlShow que acepta tres parámetros: $tabla, $item y $valor.
     static public function mdlShow($tabla, $item, $valor){
-
+        // verifica si el parámetro $item es nulo o no. Si no es nulo, entonces se prepara una consulta SQL que selecciona todas las columnas de la tabla $tabla
+        // donde la columna $item es igual al valor del parámetro $valor.
         if($item != null){
             $sentenciaSQL = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item =:$item");
 
@@ -33,13 +35,10 @@ class Sucursal{
 
             $sentenciaSQL -> execute();
 
-            return $sentenciaSQL -> fetchAll();
+            return $sentenciaSQL -> fetchAll();//devuelve una única fila de la tabla en forma de array asociativo.
         }
 
-        
-
-        
-
+    
         $sentenciaSQL -> close();
 
         $sentenciaSQL = null;
@@ -48,6 +47,7 @@ class Sucursal{
 
     static public function mdlAdd($table, $datas){
 
+        //mdlAdd que acepta dos parámetros: $table y $datas.
         $sentenciaSQL = Conexion::conectar()->prepare("INSERT INTO $table (codigo, nombre,direccion,telefono,email) VALUES
                                                                             (:codigo, :nombre, :direccion, :telefono, :email )");
 
@@ -58,7 +58,8 @@ class Sucursal{
         $sentenciaSQL->bindParam(':email', $datas["email"], PDO::PARAM_STR);
   
 
-
+        //Si la ejecución de la declaración SQL es exitosa, 
+        //la función devuelve el valor "ok". De lo contrario, devuelve "error".
 
         if($sentenciaSQL->execute()){
             
@@ -74,16 +75,17 @@ class Sucursal{
     }
 
     static public function mdlRead(){
+        //mdlRead que no acepta ningún parámetro.
         $sentenciaSQL= Conexion::conectar()->prepare("SELECT * FROM sucursal");
         $sentenciaSQL->execute();
         $listaSucursal=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
 
-        return $listaSucursal;
+        return $listaSucursal;// devuelve el array que contiene todas las filas resultantes de la consulta.
 
     }
 
     static public function mdlUpdate($table, $datas){
-
+        //mdlUpdate que acepta dos parámetros: $table y $datas.
         $sentenciaSQL = Conexion::conectar()->prepare("UPDATE $table SET nombre = :nombre, direccion = :direccion, email = :email,
                                                          telefono = :telefono, email = :email WHERE codigo = :codigo");
         
@@ -94,7 +96,7 @@ class Sucursal{
         $sentenciaSQL->bindParam(':telefono', $datas["telefono"], PDO::PARAM_STR);
         $sentenciaSQL->bindParam(':email', $datas["email"], PDO::PARAM_STR);
       
-
+        //Si la ejecución es exitosa, la función devuelve "ok". De lo contrario, devuelve "error".
         if($sentenciaSQL->execute()){
             
             return "ok";
@@ -107,12 +109,12 @@ class Sucursal{
         $sentenciaSQL = null;
 
     }
-
+    //mdlDelete que acepta dos parámetros: $table y $data.
     static public function mdlDelete($table, $data){
 
         $sentenciaSQL = Conexion::conectar()->prepare("DELETE FROM $table WHERE codigo = :codigo");
         $sentenciaSQL -> bindParam(':codigo', $data, PDO::PARAM_INT);
-
+        // Si la ejecución es exitosa, la función devuelve "ok". De lo contrario, devuelve "error".
         if($sentenciaSQL->execute()){
             return "ok";
         }else{
--- model/unitModel.php
+++ model/unitModel.php
@@ -3,9 +3,10 @@
     require_once "conexion.php";
 
     class Unit{             
-
+        //mdlShowUnit recibe tres parámetros: $tabla, $item y $valor. La variable $tabla indica la tabla de la base de datos en la que se desea hacer la consulta. La variable
+        // $item representa el nombre del campo de la tabla que se desea consultar
         static public function mdlShowUnit($tabla, $item, $valor){
-
+            //devuelve una fila de la tabla de la base de datos que coincide con el valor proporcionado en $valor y $item
             if($item != null){
                 $sentenciaSQL = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item =:$item");
 
@@ -34,12 +35,12 @@
         }
 
         static public function mdlAddUnit($table, $datas){
-
+            // recibe dos parámetros: $table, que indica la tabla en la base de datos a la que se agregarán los datos, 
+            //y $datas, que es un arreglo asociativo que contiene los valores que se agregarán a la tabla 
             $sentenciaSQL = Conexion::conectar()->prepare("INSERT INTO $table (codigo, nombre) VALUES (:codigo, :nombre)");
-
             $sentenciaSQL->bindParam(':codigo', $datas["codigo"], PDO::PARAM_STR);
             $sentenciaSQL->bindParam(':nombre', $datas["nombre"], PDO::PARAM_STR);
-
+            //Si se ejecuta sin errores, devuelve "ok", de lo contrario, devuelve "error".
             if($sentenciaSQL->execute()){
                 
                 return "ok";
@@ -52,7 +53,7 @@
             $sentenciaSQL = null;
 
         }
-
+        //mdlReadUnit no recibe ningún parámetro. Devuelve un array asociativo con los datos de todas las categorías almacenadas en la tabla categoria.
         static public function mdlReadUnit(){
             $sentenciaSQL= Conexion::conectar()->prepare("SELECT * FROM categoria");
             $sentenciaSQL->execute();
@@ -61,7 +62,8 @@
             return $listaEmpleados;
 
         }
-
+        //mdlUpdateUnit recibe como parámetros el nombre de la tabla ($table) y un arreglo con los datos a actualizar ($datas), que incluye el código de la unidad a actualizar ($datas["codigo"])
+        // y el nuevo nombre de la unidad ($datas["nombre"]).
         static public function mdlUpdateUnit($table, $datas){
 
             $sentenciaSQL = Conexion::conectar()->prepare("UPDATE $table SET nombre = :nombre WHERE codigo = :codigo");
@@ -69,7 +71,7 @@
             $sentenciaSQL->bindParam(':nombre', $datas["nombre"], PDO::PARAM_STR);
 
             $sentenciaSQL->bindParam(':cedula', $datas["cedula"], PDO::PARAM_STR);
-
+            //La función devuelve un string indicando si la operación se realizó correctamente o no: "ok" en caso de éxito y "error" en caso contrario.
             if($sentenciaSQL->execute()){
                 
                 return "ok";
@@ -82,12 +84,13 @@
             $sentenciaSQL = null;
 
         }
-
+        //mdlDeleteUnit recibe dos parámetros: $table, que es el nombre de la tabla de la que se eliminará un registro, y $data,
+        // que es el valor del campo "codigo" del registro que se desea eliminar
         static public function mdlDeleteUnit($table, $data){
 
             $sentenciaSQL = Conexion::conectar()->prepare("DELETE FROM $table WHERE codigo = :codigo");
             $sentenciaSQL -> bindParam(':codigo', $data, PDO::PARAM_INT);
-
+            //La función devuelve una cadena de texto indicando si la eliminación se realizó correctamente ("ok") o si ocurrió un error ("error").
             if($sentenciaSQL->execute()){
                 return "ok";
             }else{
